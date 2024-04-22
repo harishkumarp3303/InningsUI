09e5006a6264ae2529711f72ef753fe2400dab35
Harish kumar <harishkumarp3303@gmail.com>
2024-04-22 09:09:24 +0530

    url

--- src/app/entities/player/player.service.ts
+++ src/app/entities/player/player.service.ts
@@ -9,7 +9,7 @@ export class PlayerService {
   constructor(private http:HttpClient) { }
 
   public getAllPlayers(){
-    return this.http.get("http://localhost:8080/Player-0.0.1-SNAPSHOT/player")
+    return this.http.get("http://54.66.1.157:8080/Player-0.0.1-SNAPSHOT/player")
   }
 
 }
